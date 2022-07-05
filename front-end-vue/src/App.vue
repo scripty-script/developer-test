75929bf5c0d49ef8f8fad22d6f3a281a2e8c8aa1
scripty-script <98148272+scripty-script@users.noreply.github.com>
2022-07-06 06:45:52 +0800

    Update App.vue
    Products are now searchable and filterable.

--- front-end-vue/src/App.vue
+++ front-end-vue/src/App.vue
@@ -8,6 +8,10 @@ import DeleteProduct from './components/modals/DeleteProduct.vue'
 
 import { useCategoryStore } from '@/stores/category'
 
+import { Switch } from '@headlessui/vue'
+
+const hasNoStack = ref(false)
+
 const modalRef = ref({
     isOpen: false,
     sub: undefined,
@@ -35,9 +39,27 @@ const closeModal = () => {
 const products = useProductStore();
 const categories = useCategoryStore();
 
-const loadProducts = async () => {
+const searchHandler = (e) => {
+    e.preventDefault();
+    loadProducts(e.target.search.value, hasNoStack.value);
+}
+
+const loadProducts = async (query = undefined || '', filter = undefined) => {
     try {
-        const res = await fetch('http://localhost:8000/api/products');
+        let url = 'http://localhost:8000/api/products';
+        const params = new URLSearchParams();
+
+        if (query !== undefined && query !== '') {
+            params.append('q', query);
+        }
+
+        if (filter) {
+            params.append('filter', 'no-stock');
+        }
+
+        url += '?' + params.toString();
+
+        const res = await fetch(url);
         if (res.ok) {
             const data = await res.json();
             products.reset(data);
@@ -50,7 +72,6 @@ const loadProducts = async () => {
 
 onMounted(() => {
     loadProducts();
-
     (async () => {
         try {
             const res = await fetch('http://localhost:8000/api/categories');
@@ -61,7 +82,7 @@ onMounted(() => {
         } catch (error) {
             console.log('Something went wrong!', error);
         }
-    })()
+    })();
 })
 </script>
 
@@ -76,9 +97,21 @@ onMounted(() => {
         <section>
             <div class="container py-4 space-y-4">
                 <div class="flex justify-between">
-                    <input type="text" placeholder="Type here to search">
+                    <form @submit="searchHandler">
+                        <input type="search" placeholder="Type here to search" name="search">
+                        <div class="mt-2 flex items-center space-x-2">
+                            <Switch v-model="hasNoStack" :class="hasNoStack ? 'bg-teal-900' : 'bg-teal-600'"
+                                class="relative inline-flex h-6 w-11 items-center rounded-full">
+                                <span class="sr-only">Enable notifications</span>
+                                <span :class="hasNoStack ? 'translate-x-6' : 'translate-x-1'"
+                                    class="inline-block h-4 w-4 transform rounded-full bg-white transition duration-200 ease-in-out" />
+                            </Switch>
+                            <span class="text-sm">Has No Stack</span>
+                        </div>
+                    </form>
+
                     <div class="space-x-2">
-                        <button class="btn" @click="loadProducts">Refresh</button>
+                        <button class="btn" @click="() => { loadProducts(undefined, hasNoStack); }">Refresh</button>
                         <button class="btn-fill" @click="() => { openModal(MODAL_SUB.INSERT); }">New</button>
                     </div>
                 </div>
