fe32b7abb50acddd482d36e38473970ffb1c859a
scripty-script <98148272+scripty-script@users.noreply.github.com>
2022-07-11 09:39:10 +0800

    Update App.vue
    Correction accessing dotenv and add loading animation to button login

--- front-end-vue-unauth/src/App.vue
+++ front-end-vue-unauth/src/App.vue
@@ -5,11 +5,14 @@ const alert = ref({
     message: undefined
 })
 
+const onPending = ref(false);
+
 const submitHandler = async (e) => {
     e.preventDefault();
+    onPending.value = true;
     try {
         const xsrf = await (async () => {
-            const res = await fetch(`${process.env.API_HOST || ''}/sanctum/csrf-cookie`, {
+            const res = await fetch(`${import.meta.env.VITE_API_HOST || ''}/sanctum/csrf-cookie`, {
                 credentials: 'include'
             });
             if (res.ok) {
@@ -20,11 +23,11 @@ const submitHandler = async (e) => {
             }
         })();
 
-        const res = await fetch(`${process.env.API_HOST || ''}/api/login`, {
+        const res = await fetch(`${import.meta.env.VITE_API_HOST || ''}/api/login`, {
             method: 'POST',
             credentials: 'include',
             headers: { 'X-XSRF-TOKEN': decodeURIComponent(xsrf) },
-            body : new FormData(e.target)
+            body: new FormData(e.target)
         });
 
         if (res.ok) {
@@ -38,6 +41,8 @@ const submitHandler = async (e) => {
     } catch (error) {
         alert.value.message = "Something went wrong!";
         console.log('Something went wrong!', error);
+    } finally {
+        onPending.value = false;
     }
 }
 
@@ -57,13 +62,29 @@ const submitHandler = async (e) => {
                     </p>
                     <div>
                         <label for="email" class="block text-sm">Email</label>
-                        <input type="email" name="email" id="email" class="w-full" />
+                        <input type="email" name="email" id="email" class="w-full" required/>
                     </div>
                     <div class="mt-4">
                         <label for="password" class="block text-sm">Password</label>
-                        <input type="password" name="password" id="password" class="w-full" />
+                        <input type="password" name="password" id="password" class="w-full" required />
                     </div>
-                    <button class="btn-fill mt-6">Login</button>
+
+                    <!-- button goes here -->
+                    <button v-if="onPending"
+                        class="btn-fill mt-6 flex justify-center items-center w-full cursor-not-allowed disabled:opacity-75"
+                        type="button" disabled>
+                        <svg class="animate-spin -ml-1 mr-2 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+                            </circle>
+                            <path class="opacity-75" fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                            </path>
+                        </svg>  
+                        Verifying
+                    </button>
+                    <button v-else class="btn-fill mt-6">Login</button>
+                    <!-- button ends here -->
                 </div>
             </form>
         </section>
